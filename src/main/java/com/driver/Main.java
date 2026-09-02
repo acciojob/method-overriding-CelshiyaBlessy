629eeac8815bc39c2ab1b0b42837f4817ad9c303
CelshiyaBlessy <celshiyablesswin@gmail.com>
2026-09-02 16:27:28 +0530

    Method Overriding Assignment completed by CelshiyaBlessy

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,4 @@
 package com.driver;
-
-import java.sql.SQLOutput;
-class A{
-    String meth(){
-        return("Invoking method from class A");
-    }
-}
-class B extends A{
-    @Override
-    String meth(){
-        return("Method is overridden in Extendend class B");
-    }
-
-}
 public class Main{
     public static void main(String[] args) {
         B obj=new B();
